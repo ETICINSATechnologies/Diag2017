7c76e6d5e184608cb79ab694122f7d69f3f31f5a
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2017-03-18 22:09:48 +0100

    Accueil portrait

--- diag/www/components/home/home.controller.js
+++ diag/www/components/home/home.controller.js
@@ -5,9 +5,30 @@
         .module('diag')
         .controller('homeController', homeController);
 
-    homeController.$inject = [];
+    homeController.$inject = ['$scope'];
 
-    function homeController() {
-
+    function homeController($scope) {
+      var w = angular.element(window);
+      $scope.$watch(
+        function () {
+          return window.innerWidth;
+        },
+        function (value) {
+          $scope.windowWidth = value;
+        },
+        true
+      );
+      $scope.$watch(
+        function () {
+          return window.innerHeight;
+        },
+        function (value) {
+          $scope.windowHeight = value;
+        },
+        true
+      );
+      w.bind('resize', function(){
+        $scope.$apply();
+      });
     }
 })();
