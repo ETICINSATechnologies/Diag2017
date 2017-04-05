1e2376afb67417a84f764047457da94b74093ae0
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2017-04-05 14:19:45 +0200

    Ajout police et details etude

--- diag/www/components/projects/project-view/project-view.controller.js
+++ diag/www/components/projects/project-view/project-view.controller.js
@@ -9,8 +9,16 @@
 
   function projectViewController($scope, $state, WindowService, ProjectService) {
     $scope.WindowService = WindowService;
+
+    $scope.currentTask = 0;
+
+    $scope.setCurrentTask = function (n) {
+      $scope.currentTask = n;
+    };
+
     ProjectService.getAllProjects().success(function (data) {
       $scope.project = ProjectService.projects[$state.params.x][$state.params.y];
+      console.log($scope.project);
     }).error(function (error) {
       console.log(error);
     });
