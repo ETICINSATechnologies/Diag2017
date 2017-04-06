a77a14019d641bbb266db2a687986aa06f18af5c
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2017-04-06 13:18:25 +0200

    Corrections design diverses

--- diag/www/components/app/app.controller.js
+++ diag/www/components/app/app.controller.js
@@ -21,27 +21,6 @@
             });
         }
 
-        // .fromTemplate() method
-        var template = '<ion-popover-view>' +
-            '   <ion-header-bar>' +
-            '       <h1 class="title">My Popover Title</h1>' +
-            '   </ion-header-bar>' +
-            '   <ion-content class="padding">' +
-            '       My Popover Contents' +
-            '   </ion-content>' +
-            '</ion-popover-view>';
-
-        $scope.popover = $ionicPopover.fromTemplate(template, {
-            scope: $scope
-        });
-        $scope.closePopover = function () {
-            $scope.popover.hide();
-        };
-        //Cleanup the popover when we're done with it!
-        $scope.$on('$destroy', function () {
-            $scope.popover.remove();
-        });
-
         $scope.backToHome = function () {
             for (var i = 0; i < navIcons.length; i++) {
                 navIcons[i].click();
